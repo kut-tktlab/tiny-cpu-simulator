f956aaf18bf3fe8f1046941727c6045ec541a4b9
Morinosuke Tomihara <240344g@ugs.kochi-tech.ac.jp>
2023-06-26 16:33:41 +0900

    最初のコミット

---
+++ assets/js/app.js
@@ -0,0 +1,623 @@
+// ボタン
+const run_all_button = document.getElementById("run-all-button");
+const stop_button = document.getElementById("stop_button");
+const run_step_button = document.getElementById("run_step_button")
+const reset_button = document.getElementById("reset-button");
+
+// 実行速度
+const speed = document.getElementById("speed");
+
+// レジスタ、フラグ
+const a = document.getElementById("a");
+const b = document.getElementById("b");
+const c = document.getElementById("c");
+const ip = document.getElementById("ip");
+const zero = document.getElementById("zero");
+const carry = document.getElementById("carry");
+const negative = document.getElementById("negative");
+
+
+// エラーメッセージを表示するpタグ
+const output_error_message = document.getElementById("error_message");
+
+// メモリマップを表すフォーム
+const address_form = document.getElementById("addresses");
+
+// メモリの値
+const addresses = document.getElementsByClassName("adress");
+
+// エラーメッセージ
+let error_message = "";
+
+// 実行中かどうかの変数
+let is_running = false;
+
+// プログラム終了の命令が出たかどうかの変数
+let is_end = false;
+
+// エラーが出ているかどうかの変数
+let is_error = false;
+
+// 実行に関する変数
+let process;
+let past_address_number = null;
+let used_memories = null;
+
+// 値が代入されたメモリ
+let located_memorys = [];
+
+// レジスタと値の対応
+const registers = {
+    1: 0, 
+    2: 0, 
+    3: 0, 
+    4: 0, 
+};
+
+// フラグと値の対応
+const flags = {
+    1: false, 
+    2: false, 
+    3: false 
+};
+
+// レジスタの値の表示を更新
+const update_register = (register_number) => {
+    let num = ('00' + (registers[register_number] >>> 0).toString(16)).slice(-2);
+    switch(register_number) {
+        case 1:
+            a.textContent = num;
+            break;
+        case 2:
+            b.textContent = num;
+            break;
+        case 3:
+            c.textContent = num;
+            break;
+        default:
+            ip.textContent = num;
+            break;
+    }
+}
+
+// フラグの値の表示を更新
+const update_flag = () => {
+    for(let i = 1; i < Object.keys(flags).length + 1; i++) {
+        let value = flags[i];
+        let printed_value;
+        if (value) {
+            printed_value = "T";
+        } else {
+            printed_value = "F";
+        }
+
+        switch(i) {
+            case 1:
+                zero.textContent = printed_value;
+                break;
+            case 2:
+                carry.textContent = printed_value;
+                break;
+            default:
+                negative.textContent = printed_value;
+                break;
+        }
+    }
+}
+
+// レジスタの値を表示
+for(let i = 1; i < Object.keys(registers).length + 1; i++) {
+    update_register(i);
+}
+
+// フラグの値を表示
+for(let i = 1; i < Object.keys(flags).length + 1; i++) {
+    update_flag();
+}
+
+// エラーの関数
+const error = (message) => {
+    stop();
+    is_error = true;
+
+    // エラーメッセージ表示
+    error_message = message;
+    output_error_message.textContent = error_message;
+
+    registers[4] = 0;
+    stop();
+}
+
+// メモリの入力制限
+for (let i = 0; i < addresses.length; i++) {
+    addresses[i].oninput = () => {
+        let memory_value = addresses[i].value;
+        addresses[i].value = memory_value.replace(/[^A-Fa-f0-9]/, "");
+    }
+}
+
+// 実行停止
+const stop = () => {
+    clearInterval(process);
+    is_running = false;
+    is_end = false;
+    is_error = false;
+}
+
+// HLT命令(プログラムを終了する命令)
+const hlt = () => {
+    is_end = true;
+}
+
+// mov(即値)命令(レジスタに即値を代入する命令)
+const mov_immediate = () => {
+    // レジスタ番号を取得
+    registers[4]++;
+    let register_num = parseInt(addresses[registers[4]].value, 16);
+    
+    // レジスタ番号が違ったらエラーを表示
+    if (register_num > 3 || register_num <= 0 || isNaN(register_num)) {
+        error("レジスタの番号が違います");
+        return;
+    }
+
+    // 代入する即値を取得
+    registers[4]++;
+    let immediate = parseInt(addresses[registers[4]].value, 16);
+
+    // 即値が読めない場合はエラー
+    if (isNaN(immediate)) {
+        error("16進数で値を設定してください");
+        return;
+    }
+
+    // レジスタに代入
+    registers[register_num] = immediate;
+
+    // 代入したレジスタの表示を更新
+    update_register(register_num);
+}
+
+// mov(レジスタ)命令(レジスタにレジスタの値を代入する命令)
+const mov_register = () => {
+    // レジスタ番号を取得
+    registers[4]++;
+    let register_num1 = parseInt(addresses[registers[4]].value, 16);
+    
+    // レジスタ番号が違ったらエラーを表示
+    if (register_num1 > 3 || register_num1 <= 0 || isNaN(register_num1)) {
+        error("レジスタの番号が違います");
+        return;
+    }
+
+    // 代入するレジスタを取得
+    registers[4]++;
+    let register_num2 = parseInt(addresses[registers[4]].value, 16);
+
+    // レジスタ番号が違ったらエラーを表示
+    if (register_num2 > 3 || register_num2 <= 0 || isNaN(register_num2)) {
+        error("レジスタの番号が違います");
+        return;
+    }
+
+    // レジスタに代入
+    registers[register_num1] = registers[register_num2];
+
+    // 代入したレジスタの表示を更新
+    update_register(register_num1);
+}
+
+// add命令(足し算)、sub命令(引き算)
+const add_or_sub = (operator) => {
+    // 計算結果を代入するレジスタ番号を取得
+    registers[4]++;
+    let result_register_num = parseInt(addresses[registers[4]].value, 16);
+    
+    // レジスタ番号が違ったらエラーを表示
+    if (result_register_num > 3 || result_register_num <= 0 || isNaN(result_register_num)) {
+        error("レジスタの番号が違います");
+        return;
+    }
+
+    // 計算するレジスタ番号(1つ目)を取得
+    registers[4]++;
+    let calclated_register_num1 = parseInt(addresses[registers[4]].value, 16);
+    
+    // レジスタ番号が違ったらエラーを表示
+    if (calclated_register_num1 > 3 || calclated_register_num1 <= 0 || isNaN(calclated_register_num1)) {
+        error("レジスタの番号が違います");
+        return;
+    }
+
+    // 計算するレジスタ番号(2つ目)を取得
+    registers[4]++;
+    let calclated_register_num2 = parseInt(addresses[registers[4]].value, 16);
+    
+    // レジスタ番号が違ったらエラーを表示
+    if (calclated_register_num2 > 3 || calclated_register_num2 <= 0 || isNaN(calclated_register_num2)) {
+        error("レジスタの番号が違います");
+        return;
+    }
+
+    let result;
+    // 計算をして結果を取得
+    if (operator === "+") {
+        result = registers[calclated_register_num1] + registers[calclated_register_num2];
+    } else {
+        // 計算結果を取得
+        result = registers[calclated_register_num1] - registers[calclated_register_num2];
+    }
+
+    // 計算結果が0ならzeroフラグを立てる
+    if (result === 0) {
+        flags[1] = true;
+    } else {
+        flags[1] = false;
+    }
+
+    // 計算結果が255より大きいまたは0未満ならcarryフラグを立てる
+    if (result < 0 || result > 255) {
+        flags[2] = true;
+    } else {
+        flags[2] = false;
+    }
+
+    // 計算結果が負の値ならnegativeフラグを立てる
+    if (result < 0) {
+        flags[3] = true;
+    } else {
+        flags[3] = false;
+    }
+
+    // レジスタの値を更新
+    registers[result_register_num] = result;
+
+    // レジスタの表示を更新
+    update_register(result_register_num);
+        
+    // フラグの表示を更新
+    update_flag();
+}
+
+// cmp命令（比較命令）
+const cmp = () => {
+    // レジスタ番号を取得
+    registers[4]++;
+    let register_num1 = parseInt(addresses[registers[4]].value, 16);
+    
+    // レジスタ番号が違ったらエラーを表示
+    if (register_num1 > 3 || register_num1 <= 0 || isNaN(register_num1)) {
+        error("レジスタの番号が違います");
+        return;
+    }
+
+    // 比較するレジスタを取得
+    registers[4]++;
+    let register_num2 = parseInt(addresses[registers[4]].value, 16);
+
+    // レジスタ番号が違ったらエラーを表示
+    if (register_num2 > 3 || register_num2 <= 0 || isNaN(register_num2)) {
+        error("レジスタの番号が違います");
+        return;
+    }
+
+    // レジスタに格納されている値を取得
+    const value1 = registers[register_num1];
+    const value2 = registers[register_num2];
+
+    // 引き算
+    const result = value1 - value2;
+
+    // Zフラグ
+    if (result === 0) {
+        flags[1] = true;
+    } else {
+        flags[1] = false;
+    }
+
+    // Cフラグ、 Nフラグ
+    if (result < 0) {
+        flags[2] = true;
+        flags[3] = true;
+    } else {
+        flags[2] = false;
+        flags[3] = false;
+    }
+
+    // フラグの表示を更新
+    update_flag();
+}
+
+// b命令（ジャンプ命令）
+const jump = (conditions) => {
+    // ジャンプ先のアドレスを取得
+    registers[4]++;
+    const jump_destination_address = parseInt(addresses[registers[4]].value, 16);
+
+    // 指定したアドレスがない場合
+    if (jump_destination_address > addresses.length - 1) {
+        error("アドレスの指定に誤りがあります");
+        return;
+    }
+
+    // 条件分岐
+    let judge = false;
+    if (conditions === ">") {
+        if (!flags[1] && !flags[3]) {
+            judge = true;
+        }
+    } else if (conditions === ">=") {
+        if (!flags[3]) {
+            judge = true;
+        }
+    } else if (conditions === "==") {
+        if (flags[1]) {
+            judge = true;
+        }
+    } else if (conditions === "<=") {
+        if (flags[1] || flags[3]) {
+            judge = true;
+        }
+    } else if (conditions === "<") {
+        if (flags[3]) {
+            judge = true;
+        }
+    } else {
+        judge = true;
+    }
+
+    if (judge) {
+        registers[4] = jump_destination_address - 1;
+    }
+}
+
+// str命令（レジスタの値をメモリにコピーする命令）、ldr命令（メモリの値をレジスタにコピーする命令）
+const str_or_ldr = (argument) => {
+    // レジスタ番号を取得
+    registers[4]++;
+    let register_num1 = parseInt(addresses[registers[4]].value, 16);
+    
+    // レジスタ番号が違ったらエラーを表示
+    if (register_num1 > 3 || register_num1 <= 0 || isNaN(register_num1)) {
+        error("レジスタの番号が違います");
+        return;
+    }
+
+    // レジスタ番号を取得
+    registers[4]++;
+    let register_num2 = parseInt(addresses[registers[4]].value, 16);
+    
+    // レジスタ番号が違ったらエラーを表示
+    if (register_num2 > 3 || register_num2 <= 0 || isNaN(register_num2)) {
+        error("レジスタの番号が違います");
+        return;
+    }
+
+    // アドレスを取得
+    let read_address = registers[register_num2];
+
+    // 指定したアドレスがない場合
+    if (read_address > addresses.length - 1) {
+        error("アドレスの指定に誤りがあります");
+        return;
+    }
+
+    // 値をコピー
+    if (argument === "str") {
+        addresses[read_address].value = ('00' + registers[register_num1].toString(16)).slice(-2);
+
+        // コピー先のアドレスを保存していない場合保存する
+        if (located_memorys.indexOf(read_address) === -1) {
+            located_memorys[located_memorys.length] = read_address;
+        }
+    } else {
+        registers[register_num1] = parseInt(addresses[read_address].value, 16);
+
+        // レジスタの表示を更新
+        update_register(register_num1);
+    }
+}
+
+// 命令と使用するメモリの数
+const orders = [
+    [hlt, 1],           // 00: 終了命令
+    [mov_immediate, 3], // 01: レジスタに即値を代入する命令
+    [mov_register, 3],  // 02: レジスタにレジスタの値を代入する命令
+    [add_or_sub, 4],    // 03: 加算命令
+    [add_or_sub, 4],    // 04: 減算命令
+    [cmp, 3],           // 05: 比較命令
+    [jump, 2],          // 06: 無条件ジャンプ命令
+    [jump, 2],          // 07: ジャンプ命令(>)
+    [jump, 2],          // 08: ジャンプ命令(>=)
+    [jump, 2],          // 09: ジャンプ命令(==)
+    [jump, 2],          // 0a: ジャンプ命令(<=)
+    [jump, 2],          // 0b: ジャンプ命令(<)
+    [str_or_ldr, 3],    // 0c: メモリにレジスタの値をコピーする命令
+    [str_or_ldr, 3],    // 0d: レジスタにメモリの値をコピーする命令
+];
+
+// メモリの値に対応する命令を実行
+const output_memory = () => {
+    // エラーメッセージ初期化
+    error_message = "";
+    output_error_message.textContent = error_message;
+
+    // 読んでいるメモリのアドレスを16進数でIPレジスタに表示
+    let num = ('00' + registers[4].toString(16)).slice(-2);
+    ip.textContent = num;
+
+    // 1つ前に読んでいたメモリの色を元に戻す
+    if (past_address_number !== null) {
+        for (let i = past_address_number; i <= past_address_number + used_memories - 1; i++) {
+            // 最後のメモリを超えたアドレスを指定している場合は何もしない
+            if (i <= addresses.length - 1) {
+                addresses[i].style.backgroundColor = "";
+                addresses[i].style.borderWidth = "";
+            }
+        }
+    }
+
+    // 命令が書いてあるアドレスを保存
+    past_address_number = registers[4];
+
+    // メモリの値を10進数に変換
+    let address_value = addresses[registers[4]].value;
+    address_value = parseInt(address_value, 16);
+
+    // メモリの値が16進数か確認
+    if (isNaN(address_value)) {
+        error("16進数で値を設定してください");
+        return;
+    }
+
+    // 値に対応する命令があるか確認
+    if (!orders[address_value]) {
+        error("値に対応する命令がありません");
+        return;
+    }
+
+    // 読んでるメモリの色を変える
+    for (let i = 0; i < orders[address_value][1]; i++) {
+        // 最後のメモリを超えていないかを確認
+        if (addresses[registers[4] + i]) {
+            addresses[registers[4] + i].style.backgroundColor = "#fab387";
+        } else {
+            error("メモリマップの上限を超えました");
+            return;
+        }
+    }
+
+    // 命令を実行
+    if (address_value === 3) {
+        orders[address_value][0]("+");
+    } else if (address_value === 4) {
+        orders[address_value][0]("-");
+    } else if (address_value === 6) {
+        orders[address_value][0](null);
+    } else if (address_value === 7) {
+        orders[address_value][0](">");
+    } else if (address_value === 8) {
+        orders[address_value][0](">=");
+    } else if (address_value === 9) {
+        orders[address_value][0]("==");
+    } else if (address_value === 10) {
+        orders[address_value][0]("<=");
+    } else if (address_value === 11) {
+        orders[address_value][0]("<");
+    } else if (address_value === 12) {
+        orders[address_value][0]("str");
+    } else if (address_value === 13) {
+        orders[address_value][0]("ldr");
+    } else {
+        orders[address_value][0]();
+    }
+
+    // 命令で使用されたメモリの数を保存
+    used_memories = orders[address_value][1];
+
+    // 最後のメモリか、終了命令が出ているか、エラーが出ている場合は止める
+    if(registers[4] >= addresses.length - 1 || is_end || is_error) {
+        registers[4] = 0;
+        stop();
+    } else {
+        registers[4]++;
+    }
+}
+
+// 実行ボタンの動作
+run_all_button.onclick = () => {
+    // 0.8秒間隔で命令を実行
+    if(!is_running) {
+        is_running = true;
+
+        // 値がコピーされたメモリがある場合は初期化
+        if (located_memorys.length !== 0) {
+            for (let i = 0; i < located_memorys.length; i++) {
+                addresses[located_memorys[i]].value = "00";
+            }
+        }
+
+        // レジスタ初期化
+        for(let i = 1; i < Object.keys(registers).length + 1; i++) {
+            registers[i] = 0;
+            update_register(i);
+        }
+
+        // フラグ初期化
+        for(let i = 1; i < Object.keys(flags).length + 1; i++) {
+            flags[i] = false;
+            update_flag();
+        }
+
+        // 実行速度を取得
+        const run_speed = speed.value;
+
+        // プログラム実行
+        process = setInterval(output_memory, run_speed);
+    }
+}
+
+// ステップ実行ボタンの動作
+run_step_button.onclick = () => {
+    // 実行中に押された時の処理
+    if (is_running) {
+        stop();
+    }
+
+    // 最初のメモリを実行する場合はレジスタとフラグとメモリの色を初期化
+    if (registers[4] === 0) {
+        // 値がコピーされたメモリがある場合は初期化
+        if (located_memorys.length !== 0) {
+            for (let i = 0; i < located_memorys.length; i++) {
+                addresses[located_memorys[i]].value = "00";
+            }
+        }
+
+        // レジスタを初期化
+        for(let i = 1; i < Object.keys(registers).length + 1; i++) {
+            registers[i] = 0;
+            update_register(i);
+        }
+
+        // フラグ初期化
+        for(let i = 1; i < Object.keys(flags).length + 1; i++) {
+            flags[i] = false;
+            update_flag();
+        }
+    }
+
+    output_memory();
+}
+
+// ストップボタンの動作
+stop_button.onclick = () => {
+    if(is_running) {
+        registers[4] = 0;
+        stop();
+    }
+}
+
+// リセットボタンの動作
+reset_button.onclick = () => {
+    address_form.reset();
+
+    // メモリマップの色を初期化
+    for (let i = past_address_number; i < past_address_number + used_memories - 1; i++) {
+        addresses[i].style.backgroundColor = "";
+        addresses[i].style.borderWidth = "";
+    }
+}
+
+// メモリ入力後、enterを押したら次のメモリに移動する動作
+for(let i = 0; i < addresses.length; i++) {
+    addresses[i].addEventListener("keypress", (event) => {
+        if (event.key === "Enter") {
+            if (i < addresses.length - 1) {
+                addresses[i + 1].focus();
+                addresses[i + 1].setSelectionRange(2, 2);
+            } else {
+                addresses[i].blur();
+            }
+        }
+    })
+}
